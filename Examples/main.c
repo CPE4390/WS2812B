127e618d3d467739be64437b678c61e78ecf66ba
EET4340 <45945733+EET4340@users.noreply.github.com>
2019-01-09 12:04:07 -0700

    Initial Commit

--- Examples/main.c
+++ Examples/main.c
@@ -1,7 +1,22 @@
 
 #include <xc.h>
 #include "../WS2812B.h"
+#include "LCD.h"
+
+
+//CONFIG bits for PIC18F87J11
+#pragma config FOSC=HSPLL
+#pragma config WDTEN=OFF
+#pragma config XINST=OFF
+
 
 void main(void) {
-    return;
+    OSCTUNEbits.PLLEN = 1;
+    LCDInit();
+    LCDClear();
+    InitWS2812();
+    
+    while (1) {
+        
+    }
 }
