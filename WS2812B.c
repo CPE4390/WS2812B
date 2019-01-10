a9f3732255416fcf0b0de30691560f90614f8ce4
bemcg <bemcg@BradLaptop.hsd1.co.comcast.net>
2019-01-09 19:32:33 -0700

    Initial Commit

--- WS2812B.c
+++ WS2812B.c
@@ -2,13 +2,23 @@
 #include "WS2812B.h"
 
 void InitWS2812(void) {
-
+    TRISDbits.TRISD4 = 0;
+    SSP2STATbits.CKE = 1;
+    SSP2CON1bits.CKP = 0; //SPI mode 0,0
+    SSP2CON1bits.SSPM = 0b0000; //SPI Master - FOSC/4 = 8 MHz
+    SSP2CON1bits.SSPEN = 1; //Enable MSSP
 }
 
 void SetColor(RGB color, int numLED) {
-    
+    unsigned char buff[30];
+  
+    }
+//    SSP2BUF = byte; //transmit byte
+//    while (!PIR3bits.SSP2IF); //Wait until completed
+//    PIR3bits.SSP2IF = 0; //Clear flag so it is ready for next transfer
+//    r = SSP2BUF; //read received byte
 }
 
 void SetColors(RGB *colors, int numLED) {
-    
+
 }
